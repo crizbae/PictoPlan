8d3eabc4873d42163ee4cd1baec4ffbf2ea57669
crizbae <crboese@gmail.com>
2023-11-04 17:49:49 -0400

    holy guacamole
    yeah i do a little guacing

---
+++ backend/helper/rename.py
@@ -0,0 +1,24 @@
+import os
+
+import uuid
+
+
+
+filepath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+path = "/backend/images/textbook/"
+filepath += path
+
+counter = 1
+for filename in os.listdir(filepath):
+    
+    old_file = os.path.join(filepath, filename)
+    
+    
+    myuuid = uuid.uuid4()
+    myuuid = ''.join(str(myuuid).split('-'))
+
+    new_file = os.path.join(filepath, myuuid + "-1" + "-" + str(counter) + ".png")
+
+
+    os.rename(old_file, new_file)
+    counter+= 1
