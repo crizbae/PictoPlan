fcd0b2ffe9fb64ab97a1e548534fe80976e8b44b
Yash Kaul <kauly.cs@gmail.com>
2023-11-05 11:35:06 -0500

    BIG POGGERS COMMIT

--- backend/processor_api/routes.py
+++ backend/processor_api/routes.py
@@ -1,15 +1,29 @@
 from fastapi import APIRouter, Depends, HTTPException    
-from processor import processor                      
+from processor import processor  
+import requests        
+import json      
+import ast      
 
 router = APIRouter()
 
 @router.post("/process/")
 async def create_item(b64array: dict):
+    print("Part1!")
     if len(b64array) == 0:
         raise HTTPException(status_code=400, detail="Invalid request")
+    uuid = list(b64array.keys())[0]
+    uuid = uuid.split('-')[0]
     lessons = processor(b64array)
+    print("Part2!")
     if len(lessons) == 0:
         raise HTTPException(status_code=404, detail="GPT failed")
-    
-    # call to backend?
-    return lessons
+    url = 'https://mongo.pictoplan.org/item' 
+    print(lessons)
+    lessons = [json.loads(lesson) for lesson in lessons]
+    for lesson in lessons:
+        lesson['SessionId'] = uuid
+        response = requests.post(url, json=lesson)
+
+    url = 'https://mongo.pictoplan.org/item/session/' + uuid
+    response = requests.get(url).json()
+    return response
